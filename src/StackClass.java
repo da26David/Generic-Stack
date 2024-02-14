c3fc4d76014a518edd7d0fe10c9d5a58feae5ce0
da26David <118820917+da26David@users.noreply.github.com>
2024-02-14 09:59:59 +0100

    Added Constructors

--- src/StackClass.java
+++ src/StackClass.java
@@ -1,2 +1,10 @@
 public class StackClass <T>{
+    private T[] stack;
+    public StackClass(){
+        stack = (T[])new Object[5];
+    }
+    public StackClass(int size){
+        stack =(T[]) new Object[size];
+    }
+
 }
